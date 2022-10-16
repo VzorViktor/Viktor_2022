dffbce83e4fec678bbbb47a71bc868536a1ee2cd
Viktor Bondarovich <ponkjkeee@gmail.com>
2022-10-17 00:01:52 +0300

    Blog (Python, Django)

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # 'hello_world',
-    'project',
+    'project.apps.ProjectConfig',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
