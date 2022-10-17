c609b3f7f29e48fabefbc5528ec6fd4b4a39689e
Viktor Bondarovich <ponkjkeee@gmail.com>
2022-10-17 21:32:47 +0300

    Portfolio ready

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -106,11 +106,14 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-Ru'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
+
+USE_L10N = False
+
 
 USE_TZ = True
 
