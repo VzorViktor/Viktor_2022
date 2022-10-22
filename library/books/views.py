6fa7e5f0d8b53c973ed27796c06b8a8effb91da9
Viktor Bondarovich <ponkjkeee@gmail.com>
2022-10-23 00:22:27 +0300

    Book Part 3, Bootstrap, static and other

--- library/books/views.py
+++ library/books/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Book Post 1',
+        'content': 'First post content',
+        'date_posted': 'October 22, 2022'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Book Post 2',
+        'content': 'Second post content',
+        'date_posted': 'October 23, 2022'
+    }
+]
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'books/home.html', context)
+
+
+def about(request):
+    return render(request, 'books/about.html', {'title': 'About'})
+
+
